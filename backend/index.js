dc69889f40939029de37f1256747ffb8d7663fc0
tushhr <tusharmaheshwaritm6@gmail.com>
2024-05-03 07:45:54 +0530

    feat: add mongodb server

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,13 @@
 import Fastify from "fastify";
+import mongoose from "mongoose";
 
 const fastify = Fastify({ logger: true })
+
+try {
+  mongoose.connect('mongodb://localhost:27017/todos_db');
+} catch(err) {
+  console.log(err)
+}
 
 fastify.listen({ port: 5000 }, (err, address) => {
   if(err) {
